9c1dae1cf794d9985989fe7db50176373e5e9967
Wiley Hunt <whunt@bu.edu>
2021-09-30 12:05:35 -0400

    Added functions for read and sim

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,23 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+
+    X = []
+    with open(csv_file_path, "r") as f: #Open File
+        data = f.readlines() # Extract Readlines object
+        for line in data: # Iterate through each line, splitting entries by ','
+            line = line.strip().replace('"', '').split(',')
+            converted_line = []
+
+            #Convert entries to ints, floats, or strings
+            for el in line:
+                if el.isdecimal():
+                    converted_line.append(int(el))
+                elif el.isdigit():
+                    converted_line.append(float(el))
+                else:
+                    converted_line.append(str(el))
+        
+            X.append(converted_line)
+
+    return X
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -5,12 +5,39 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+
+    if x == [] or y == []:
+        raise ValueError("lengths must not be zero")
+
+    #Source: https://www.statology.org/manhattan-distance-python/
+    return sum(abs(val1-val2) for val1, val2 in zip(x,y))
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    x_set = set(x)
+    y_set = set(y)
+    if x == [] or y == []:
+        raise ValueError("lengths must not be zero")
+    dist = (len(x_set.union(y_set)) - len(x_set.intersection(y_set)))/len(x_set.union(y_set))
+    return dist
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+
+    #Check for edge cases
+    if x == [] or y == []:
+        raise ValueError("lengths must not be zero")
+    elif len(x) != len(y):
+        raise ValueError("lengths must be equal")
+
+    #Get dot product
+    dot_prod = sum(abs(val1*val2) for val1, val2 in zip(x,y))
+
+    #Calculate X and Y lengths
+    x_len = (sum(el**2 for el in x))**(1/2)
+    y_len = (sum(el**2 for el in y))**(1/2)
+
+    #Calculate cos
+    cos = dot_prod/(x_len * y_len)
+
+    return cos
 
 # Feel free to add more
