a8a40de74baa72026a72ddaf60ccb731ff0eb396
Wiley Hunt <whunt@bu.edu>
2021-10-07 11:46:36 -0400

    Updated k means function

--- 02-library/cs506/kmeans.py
+++ 02-library/cs506/kmeans.py
@@ -11,7 +11,22 @@ def point_avg(points):
     
     Returns a new point which is the center of all the points.
     """
-    raise NotImplementedError()
+    if len(points) == 0:
+        return []
+
+    num_points = len(points)
+    num_entries = len(points[0])
+
+    avg = [0]*num_entries
+
+    for idx, el in enumerate(points):
+        for i in range(len(el)):
+            avg[i] += el[i]
+    
+    for idx, element in enumerate(avg):
+        avg[idx] = element/num_points
+
+    return avg
 
 
 def update_centers(dataset, assignments):
@@ -21,7 +36,28 @@ def update_centers(dataset, assignments):
     Compute the center for each of the assigned groups.
     Return `k` centers in a list
     """
-    raise NotImplementedError()
+    k = len(set(assignments))
+
+    dim = len(dataset[0])
+
+    #Allocated List for return
+    centroids = []
+
+    #Iterate through the samples in each cluster and take cluster mean
+    for label in range(k):
+        cluster_indices = [idx for idx, el in enumerate(assignments) if el == label ]
+        avg_array = [0 for i in range(dim)]
+        for index in cluster_indices:
+            element = dataset[index]
+            avg_array = [a +b for a, b in list(zip(avg_array, element))]
+        
+        cluster_avg = [feature/len(cluster_indices) for feature in avg_array]
+
+
+        # cluster_avg = sum([dataset[idx] for idx in cluster_indices])/len(cluster_indices)
+        centroids.append(cluster_avg)
+
+    return centroids
 
 def assign_points(data_points, centers):
     """
@@ -43,20 +79,35 @@ def distance(a, b):
     """
     Returns the Euclidean distance between a and b
     """
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(a)):
+        res += (a[i] - b[i])**2
+    return res**(1/2)
+
 
 def distance_squared(a, b):
-    raise NotImplementedError()
+
+    return distance(a,b)**2
 
 def generate_k(dataset, k):
     """
     Given `data_set`, which is an array of arrays,
     return a random set of k points from the data_set
     """
-    raise NotImplementedError()
+    import random
+    #Extract a list of K random elements from dataset
+    centroids = random.sample(list(dataset), k)
+
+    return centroids
 
 def cost_function(clustering):
-    raise NotImplementedError()
+    cost = 0
+    for cluster_id in clustering.keys():
+        cluster = clustering.get(cluster_id)
+        centroid = point_avg(cluster)
+        for entry in cluster:
+            cost += distance_squared(entry, centroid)
+    return cost
 
 
 def generate_k_pp(dataset, k):
@@ -66,7 +117,20 @@ def generate_k_pp(dataset, k):
     where points are picked with a probability proportional
     to their distance as per kmeans pp
     """
-    raise NotImplementedError()
+
+    centroids = random.sample(list(dataset), 1)
+
+    while(len(centroids) < k):
+        max_d = 0
+        max_item = []
+        for item in dataset:
+            distance = sum([distance_squared(item, centroid) for centroid in centroids])
+            if distance > max_d:
+                max_d = distance
+                max_item = item
+        centroids.append(max_item)
+    
+    return centroids
 
 
 def _do_lloyds_algo(dataset, k_points):
